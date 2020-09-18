cd2432a7c726d7ee7d109ba902449905c6ece2db
sebug <sebastian.gfeller@gmail.com>
2020-09-18 14:28:14 +0200

    Showing file

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace construction_checklist
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,12 @@ namespace construction_checklist
 
             app.UseRouting();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
