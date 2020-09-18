dd41f7e5cad03cca8f58e5b4880c28731018b159
sebug <sebastian.gfeller@gmail.com>
2020-09-18 14:46:42 +0200

    Checklist basics

---
+++ Controllers/ConfirmConstructionController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using construction_checklist.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace construction_checklist.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfirmConstructionController : ControllerBase
+    {
+        public bool Post([FromForm] ConstructionChecklist checklist)
+        {
+            return true;
+        }
+    }
+}
---
+++ Models/ConstructionChecklist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace construction_checklist.Models
+{
+    public class ConstructionChecklist
+    {
+        public string Construction { get; set; }
+
+        public string Eclairage { get; set; }
+        public IFormFile PhotoEclairage { get; set; }
+        public string CommentsEclairage { get; set; }
+
+        public string Hygrometrie { get; set; }
+        public IFormFile PhotoHygrometrie { get; set; }
+        public string CommentsHygrometrie { get; set; }
+    }
+}
